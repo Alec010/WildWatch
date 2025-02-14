823d5be61c40ece32eb1b97080d5fa46663e741e
LAB23_USER ON WS09 <L23X14W09@citulab.net>
2025-02-14 14:16:06 +0800

    Add frontend and backend directories

---
+++ backend/wildwatch/wildwatch/src/main/java/edu/cit/wildwatch/WildwatchApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.wildwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildwatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildwatchApplication.class, args);
+	}
+
+}
---
+++ backend/wildwatch/wildwatch/src/test/java/edu/cit/wildwatch/WildwatchApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.wildwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildwatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
