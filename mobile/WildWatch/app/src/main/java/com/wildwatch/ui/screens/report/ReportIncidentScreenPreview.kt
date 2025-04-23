20b7fedb97a03a672e488b7c9fae6b7527a275f5
Jermaine Gadiano <152935237+Dazzzaiii@users.noreply.github.com>
2025-04-23 13:01:41 +0800

    ready changes before merging reporting feature

--- mobile/WildWatch/app/src/main/java/com/wildwatch/ui/screens/report/ReportIncidentScreenPreview.kt
+++ mobile/WildWatch/app/src/main/java/com/wildwatch/ui/screens/report/ReportIncidentScreenPreview.kt
@@ -1,13 +1,20 @@
 package com.wildwatch.ui.screens.report
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
+import com.wildwatch.api.RetrofitClient
+import com.wildwatch.repository.IncidentRepository
 import com.wildwatch.ui.theme.WildWatchTheme
+import com.wildwatch.viewmodel.IncidentFormViewModel
 
 @Preview(showBackground = true)
 @Composable
-fun ReportIncidentScreenPreview() {
-    WildWatchTheme {
-        ReportIncidentScreen()
-    }
+fun PreviewReportIncidentScreen() {
+    val repo = IncidentRepository(RetrofitClient.incidentApi)
+    val viewModel = remember { IncidentFormViewModel(repo) }
+
+    EvidenceWitnessScreen(
+        formViewModel = viewModel
+    )
 }
