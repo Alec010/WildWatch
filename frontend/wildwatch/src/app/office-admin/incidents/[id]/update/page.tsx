f09fbd1d90173e5951a42b6b80f2d02e7c283c05
Alec Arela <alec.arela@cit.edu>
2025-05-01 04:48:35 +0800

    last nani

--- frontend/wildwatch/src/app/office-admin/incidents/[id]/update/page.tsx
+++ frontend/wildwatch/src/app/office-admin/incidents/[id]/update/page.tsx
@@ -1,16 +1,15 @@
 import { OfficeAdminSidebar } from "@/components/OfficeAdminSidebar"
 import { UpdateIncidentClient } from "@/app/office-admin/incidents/[id]/update/UpdateIncidentClient"
 
-type Props = {
+export default async function UpdateIncidentPage({
+  params,
+}: {
   params: { id: string }
-  searchParams: { [key: string]: string | string[] | undefined }
-}
-
-export default async function UpdateIncidentPage(props: Props) {
+}) {
   return (
     <div className="flex h-screen">
       <OfficeAdminSidebar />
-      <UpdateIncidentClient incidentId={props.params.id} />
+      <UpdateIncidentClient incidentId={params.id} />
     </div>
   )
 } 
