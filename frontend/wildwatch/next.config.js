77b4500f82d38c0a6cfd611cb9cb078e109b4d8b
Alec Arela <alec.arela@cit.edu>
2025-10-28 00:02:28 +0800

    Refactor image configuration: consolidate remote patterns in next.config.js and remove unused config files

--- frontend/wildwatch/next.config.js
+++ frontend/wildwatch/next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
         hostname: 'jcldwuryjuqtrbsqlgoi.supabase.co',
         pathname: '/storage/v1/object/public/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'wildwatch-9djc.onrender.com',
+        pathname: '/api/images/**',
+      },
     ],
     unoptimized: true,
   },
